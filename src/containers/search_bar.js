f3af18f628ae5d87dcfc0e157d386c8ff1d14905
Taylor Pechacek <taylortpechacek@gmail.com>
2017-05-14 18:12:14 +1000

    finished my weather app, check it out

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -28,7 +28,7 @@ class SearchBar extends Component {
 
 	render () {
 		return (
-			<form onSubmit={this.onFormSubmit} className="input group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input
 					placeholder="Get a five-day forecast in your favorite cities"
 					className="form-control"
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
 
 class WeatherList extends Component {
+	
 	renderWeather(cityData) {
 		const name = cityData.city.name;
+		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * (9/5) - 459.67);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		
+
+		// const lon = cityData.city.coord.lon;
+		// const lat = cityData.city.coord.lat;
+
+		// ES6 version of above.
+		const { lon, lat } = cityData.city.coord;
 
 		return (
 			<tr key={name}>
-				<td>{name}</td>
+				<td><GoogleMap lon={lon} lat={lat} /></td>
+				<td><Chart data={temps} color="orange" units="F" /></td>
+				<td><Chart data={pressures} color="green" units="hPa" /></td>
+				<td><Chart data={humidities} color="black" units="%" /></td>
 			</tr>
 		);
 	}
@@ -20,9 +37,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (F)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 				<tbody>
